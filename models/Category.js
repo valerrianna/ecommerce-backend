06919741c0ce7ec0f54e97cfc2eafc387adead6b
Valerie <valerie.tram.anh.n@gmail.com>
2021-11-17 22:00:23 -0500

    add category columns

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,38 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isDecimal: true //PLEASE CHECK AND CORRECT ME
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue = 10,
+      validates: {
+        int = true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    },
   },
   {
     sequelize,
